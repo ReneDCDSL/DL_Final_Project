2b3fce13c91b055dbf69e83b52d4855ef9e57a40
ReneDCDSL <renedcdsl@hotmail.fr>
2021-12-15 21:23:04 +0100

    trying to implement class SGD

---
+++ Rene/from thiago/p2/model.py
@@ -0,0 +1,53 @@
+class Module(object):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+    def backward(self, grad):
+        pass
+
+    def parameters(self):
+        return []
+
+
+class Sequential(Module):
+    def __init__(self, *modules):
+        super(Sequential, self).__init__()
+        self.module_lst = []
+        for module in modules:
+            self.module_lst.append(module)
+
+    def forward(self, x):
+        for module in self.module_lst:
+            x = module.forward(x)
+        return x
+
+    def backward(self, grad):
+        for module in reversed(self.module_lst):
+            grad = module.backward(grad)
+        return grad
+
+'''    def SGD(self, eta, momentum):
+        for module in self.module_lst:
+            module.SGD(eta, momentum)'''
+            
+    def update_parameters(self, eta):
+        for module in self.module_lst:
+            module.update_parameters(eta)
+            
+    def parameters(self):
+        lst = []
+        for module in self.module_lst:
+            lst.append(module.parameters)
+        return lst
+
+    def zero_gradient(self):
+        for module in self.module_lst:
+            module.zero_gradient()
+        return
+
+    def reset_parameters(self):
+        for module in self.module_lst:
+            module.reset_parameters()
